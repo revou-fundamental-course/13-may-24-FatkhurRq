e9a4fd052443c3840c07dde75ffd5ff0a8318247
FatkhurRq <fatkhurrozaqman03@gmail.com>
2024-05-23 19:18:57 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
 // Ini SCRIPT JS
+function countButtonKeliling() {
+  var sisiA = parseFloat(document.getElementById("SisiA").value)
+  var sisiB = parseFloat(document.getElementById("SisiB").value)
+  var sisiC = parseFloat(document.getElementById("SisiC").value)
+
+  var keliling = sisiA + sisiB + sisiC
+
+  document.getElementById("keliling").innerHTML = keliling
+}
+
+function resetButtonKeliling (){
+    document.getElementById('SisiA').value=''
+    document.getElementById('SisiB').value=''
+    document.getElementById('SisiC').value=''
+    document.getElementById('keliling').innerHTML = ''
+}
+
+function countButtonLuas() {
+    var alas = document.getElementById('alas').value
+    var tinggi = document.getElementById('tinggi').value
+    var luas = alas * tinggi * 0.5
+
+    document.getElementById('luas').innerHTML = luas
+}
+
+function resetButtonLuas (){
+     document.getElementById('alas').value=''
+     document.getElementById('tinggi').value=''
+     document.getElementById('luas').innerHTML = ''
+}
